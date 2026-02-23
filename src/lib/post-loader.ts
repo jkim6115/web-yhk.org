86b2b0a759404b1c5b6435fa2318f97643584665
김요한 <yhk6115@eco.co.kr>
2026-02-23 15:29:20 +0900

    markdown 구문 확장

--- src/lib/post-loader.ts
+++ src/lib/post-loader.ts
@@ -3,6 +3,7 @@ import path from "path";
 import matter from "gray-matter";
 import { unified } from "unified";
 import remarkParse from "remark-parse";
+import remarkGfm from "remark-gfm";
 import remarkRehype from "remark-rehype";
 import rehypeHighlight from "rehype-highlight";
 import rehypeStringify from "rehype-stringify";
@@ -41,6 +42,7 @@ export const getPostData = unstable_cache(
 
     const processedContent = await unified()
       .use(remarkParse)
+      .use(remarkGfm)
       .use(remarkRehype)
       .use(rehypeHighlight)
       .use(rehypeStringify)
